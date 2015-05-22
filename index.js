3cd3b062ced4125edf679bf202c8b28b0acb2cfe
Les Orchard <me@lmorchard.com>
2015-05-22 15:25:56 -0400

    More progress
    * Attempt to normalize handling of output options
    
    * New commands: launch, findapp
    
    * forwardports will now forward all by default if no devices given
    
    * All command modules now return promises, index.js handles error output
      as part of init()

--- index.js
+++ index.js
@@ -5,20 +5,26 @@ var package = require(__dirname + '/package.json');
 program.version(package.version)
   .option('-D, --debug', 'enable debugging and debug output')
   .option('-q, --quiet', 'quiet output, except for errors')
-  .option('-v, --verbose', 'enable verbose data output');
+  .option('-j, --json', 'JSON data output')
+  .option('-v, --verbose', 'verbose output')
+  .option('-nc, --nocolors', 'no ANSI colors in JSON output');
 
-function init (next) {
+// Common command init wrapper with error reporting.
+function init (cmd) {
   return function () {
     var args = Array.prototype.slice.call(arguments, 0);
-    next.apply(program, args);
+    cmd.apply(program, args)
+      .catch(console.error);
   }
 }
 
+// Load up all the command modules.
 var cmds = requireDir('./lib/commands');
 for (name in cmds) {
   cmds[name](program, init);
 }
 
+// Export the CLI driver to bootstrap script
 module.exports = function () {
   program.parse(process.argv);
   if (!process.argv.slice(2).length) {
